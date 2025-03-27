47776303d5a48effb3174861ffbe31743cf3fa61
qbrentz <qbrentz@vcu.edu>
2025-03-27 11:24:59 -0400

    Delete src directory

--- src/main/java/com/example/medadherence/repositories/MedicationLogRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.medadherence.repositories;
-
-import com.example.medadherence.models.MedicationLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicationLogRepository extends JpaRepository<MedicationLog, Long> {}
